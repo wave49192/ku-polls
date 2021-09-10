8eb111db2a761a5af4284f0eb93d157127b97a01
wave49192 <sirawich.tu@ku.th>
2021-09-10 23:06:47 +0700

    add two methods Question

--- polls/models.py
+++ polls/models.py
@@ -15,9 +15,19 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        now = timezone.now()
+        return now >= self.pub_date
+
+    def can_vote(self):
+        now = timezone.now()
+        return self.end_date >= now >= self.pub_date
+
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -26,4 +36,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
