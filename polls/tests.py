ee6ad60e935f56b080fc5cb543295f7561d8c5d7
wave49192 <sirawich.tu@ku.th>
2021-09-11 17:54:18 +0700

    add unit tests for is_published and can_vote

--- polls/tests.py
+++ polls/tests.py
@@ -48,6 +48,49 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
+    def test_is_published_with_old_question(self):
+        """
+        is_published returns True for whose pub_date is older than current time
+        """
+        time = timezone.now() - datetime.timedelta(hours=24)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.is_published(), True)
+
+    def test_is_published_with_future_question(self):
+        """
+        is_published returns False for whose pub_date is newer than current time
+        """
+        time = timezone.now() + datetime.timedelta(hours=24)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.is_published(), False)
+
+    def test_can_vote_with_in_voting_range_question(self):
+        """
+        can_vote returns True when currently in voting range of time.
+        """
+        pub_date = timezone.now() - datetime.timedelta(days=10)
+        end_date = timezone.now() + datetime.timedelta(days=10)
+        published_question = Question(pub_date=pub_date, end_date=end_date)
+        self.assertIs(published_question.can_vote(), True)
+
+    def test_can_vote_with_future_question(self):
+        """
+        can_vote returns False when the question is not published yet.
+        """
+        pub_date = timezone.now() + datetime.timedelta(days=10)
+        end_date = timezone.now() + datetime.timedelta(days=20)
+        future_question = Question(pub_date=pub_date, end_date=end_date)
+        self.assertIs(future_question.can_vote(), False)
+
+    def test_can_vote_with_expired_question(self):
+        """
+        can_vote returns False when the question is expired.
+        """
+        pub_date = timezone.now() - datetime.timedelta(days=20)
+        end_date = timezone.now() - datetime.timedelta(seconds=1)
+        expired_question = Question(pub_date=pub_date, end_date=end_date)
+        self.assertIs(expired_question.can_vote(), False)
+
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
@@ -83,16 +126,12 @@ class QuestionIndexViewTests(TestCase):
 
     def test_future_question_and_past_question(self):
         """
-        Even if both past and future questions exist, only past questions
-        are displayed.
+        Even if both past and future questions exist, only past questions are displayed
         """
         create_question(question_text="Past question.", days=-30)
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
-        self.assertQuerysetEqual(
-            response.context['latest_question_list'],
-            ['<Question: Past question.>']
-        )
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question.>'])
 
     def test_two_past_questions(self):
         """
