0d764f72ee1ea3725815308f67ffbef2d279847f
wave49192 <sirawich.tu@ku.th>
2021-09-11 19:57:25 +0700

    remove function get_queryset outside IndexView class

--- polls/views.py
+++ polls/views.py
@@ -51,13 +51,3 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-
-def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
-
