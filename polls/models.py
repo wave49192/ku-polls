e9f18c41d8a82c1279fc6e650e2eddad7783bce0
wave49192 <sirawich.tu@ku.th>
2021-09-11 17:53:30 +0700

    add null=True in end_date attribute and add end_date migration file

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('ending date')
+    end_date = models.DateTimeField('ending date',null=True)
 
     def __str__(self):
         return self.question_text
