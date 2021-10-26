ac62086502f9ae0ee2981b2ac49c65482d7aad0d
wave49192 <sirawich.tu@ku.th>
2021-10-26 20:45:16 +0700

    add more auth path

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,5 @@
 """All urls variable are stored here."""
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote')]
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('accounts/', include('django.contrib.auth.urls')), ]
