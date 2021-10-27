61f9e4f32c8be4fbf4bbe38c4dc1f3f7eb9fad6e
wave49192 <sirawich.tu@ku.th>
2021-10-27 20:39:13 +0700

    create new model(Vote) and omit vote attribute in Choice class

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -41,8 +42,18 @@ class Choice(models.Model):
 
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    # votes = models.IntegerField(default=0)
 
     def __str__(self):
         """If this function is called it should returns choices."""
         return self.choice_text
+
+    @property
+    def votes(self):
+        """Return votes in that choice."""
+        return Vote.objects.filter(choice=self).count()
+
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, default=0)
