b4c05e32a735cd893ca7200bba79bc9899da4214
wave49192 <sirawich.tu@ku.th>
2021-10-27 22:07:28 +0700

    disable button for voted choice

--- mysite/views.py
+++ mysite/views.py
@@ -14,7 +14,7 @@ def signup(request):
             raw_passwd = form.cleaned_data.get('password')
             user = authenticate(username=username, password=raw_passwd)
             login(request, user)
-        return redirect('polls')
+        return redirect('polls:index')
         # what if form is not valid?
         # we should display a message in signup.html
     else:
